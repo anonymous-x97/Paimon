8ca964e60511f30ea49dc9950e80e120e06ad739
Ryuk <90273333+anonymous-x97@users.noreply.github.com>
2022-10-21 17:30:27 +0530

    Update utils.sh

--- init/utils.sh
+++ init/utils.sh
@@ -17,7 +17,7 @@ getPythonVersion() {
         [[ $count -gt $maxPVer ]] && break
     done
     local ptn='s/Python (3\.[0-9]{1,2}\.[0-9]{1,2}).*/\1/g'
-    declare -gr pVer=$(sed -E "$ptn" <<< "$(python3.$count -V 2> /dev/null)")
+    declare -gr pVer=python3.10
 }
 
 log() {
